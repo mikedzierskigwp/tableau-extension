bd796f9b74e19df03c64015fffa179b86fc46078
mikedzierski <michal.kedzierski@grupawp.pl>
2022-06-07 16:09:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
-import logo from './wp_logo.png';
-import './App.css';
-import React, { useState, useEffect } from 'react'
-/*global tableau*/
+import React, { useEffect } from "react";
+import { Button } from '@tableau/tableau-ui';
+const { tableau } = window;
+
 
 function App() {
-
-  const [dashboardName, setDashboardName] = useState('')
-  // eslint-disable-next-line
-  const [sheetNames, setSheetNames] = useState([]);
-
   useEffect(() => {
-    tableau.extensions.initializeAsync().then(() => {
-      const sheetNames = tableau.extensions.dashboardContent.dashboard.worksheets.map(worksheet => worksheet.name);
-
-      setSheetNames(sheetNames);
-      const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
-      setDashboardName(dashboardName);
-
-    });
+    tableau.extensions.initializeAsync();
   }, []);
 
+  async function updateParameter(param_name, value) {
+    tableau.extensions.dashboardContent.dashboard.getParametersAsync().then((parameters) => {
+      parameters.forEach((param) => {
+        if (param.name === param_name) {
+          param.changeValueAsync(value);
+        }
+      });
+    });
+  }
+
+  async function getValsForAPI() {
+    tableau.extensions.dashboardContent.dashboard.worksheets.map(worksheet =>
+      worksheet.getFiltersAsync().then(function (filters) {
+
+        filters.forEach((filter) => {
+          // /\s+/g - remove whitespace
+          if (filter.worksheetName === 'VIMP - istotność') {
+            let filterName = "ep" + filter.fieldName.replace(/\s+/g, '_').trim();
+            let filterVals;
+
+            // /,\s*$/ - remove trailing comma
+            if (filter._filterType === "categorical") {
+              if (!filter.isAllSelected) {
+                filterVals = filter.appliedValues.reduce((acc, val) => acc + val._formattedValue + ',', '');
+                updateParameter(filterName, filterVals.replace(/,\s*$/, ""))
+              } else {
+                filterVals = 'All';
+                updateParameter(filterName, filterVals.replace(/,\s*$/, ""))
+
+              };
+            } else if (filter._filterType === "range") {
+              filterVals = "min:" + filter.minValue.formattedValue + "," + "max:" + filter.maxValue.formattedValue
+              updateParameter(filterName, filterVals.replace(/,\s*$/, ""))
+            }
+          };
+        });
+
+      }
+      ));
+  }
+
+  async function handleClick() {
+    updateParameter('isNewRequest', Math.random())
+    getValsForAPI()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {dashboardName}
-        </p>
-      </header>
+    <div>
+      <button onClick={handleClick}>GO</button>
     </div>
   );
+
 }
 
 export default App;
